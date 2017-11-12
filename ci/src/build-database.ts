4ac24e45359ca8fa7c4e5e95655eae4eaf77d395
Levente Löki <leventeloki@gmail.com>
2017-11-12 11:19:07 +0100

    Build database

--- ci/src/build-database.ts
+++ ci/src/build-database.ts
@@ -3,11 +3,44 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as colors from 'colors/safe'
 
+const sourceFile = path.resolve(dirs.projectRoot, dirs.database, 'db.txt')
+const lines = fs.readFileSync(sourceFile, 'utf-8').toString().split('\n')
+
+const db = []
+var current = {
+    hashtag: '',
+    description: '',
+    img: '',
+    url: ''
+}
+
+lines.forEach((line: string, idx) => {
+    if (idx % 5 === 0) {
+        current.hashtag = line.trim()
+    }
+    if (idx % 5 === 1) {
+        current.description = line.trim()
+    }
+    if (idx % 5 === 2) {
+        current.img = line.trim()
+    }
+    if (idx % 5 === 3) {
+        current.url = line.trim()
+    }
+    if (idx % 5 === 4) {
+        db.push(current)
+        current = {
+            hashtag: '',
+            description: '',
+            img: '',
+            url: ''
+        }
+    }
+})
 
 const targetFile = path.resolve(dirs.projectRoot, dirs.database, 'db.json')
 const targetFileDisplay = path.relative(dirs.projectRoot, targetFile)
 
-const db = []
 
 fs.writeFileSync(targetFile, JSON.stringify(db))
 
